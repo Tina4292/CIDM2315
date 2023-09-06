9aa0376d070925aa25513d2ac78ae66a183f98af
Tina Coppedge <143367390+Tina4292@users.noreply.github.com>
2023-09-06 13:21:52 -0500

    Upload Homework 2 and Homework 2 bonus

---
+++ Homework2Bonus.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework2Bonus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input a year: ");
+        string year = Console.ReadLine();
+        int year_int = Convert.ToInt16(year);
+
+        if(year_int% 400==0){
+            Console.WriteLine($"{year_int} is a leap year");
+        }
+        else if(year_int% 100==0){
+            if(year_int% 400==0){
+                Console.WriteLine($"{year_int} is a leap year");
+            }
+            else{
+                Console.WriteLine($"{year_int} is not a leap year");
+            }
+        }
+        else if(year_int% 4==0){
+            if(year_int% 100==0){
+                if(year_int% 400==0){
+                    Console.WriteLine($"{year_int} is a leap year");
+                }
+                else{
+                    Console.WriteLine($"{year_int} is not a leap year");
+                }
+            }
+            else{
+                Console.WriteLine($"{year_int} is a leap year");
+            }
+        }
+        else{
+            Console.WriteLine($"{year_int} is not a leap year");
+        }
+    }
+}
---
+++ Homework2Q2.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework2Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input the first number: ");
+        string n1 = Console.ReadLine();
+        int n1_int = Convert.ToInt16(n1);
+
+        Console.WriteLine("Please input the second number: ");
+        string n2 = Console.ReadLine();
+        int n2_int = Convert.ToInt16(n2);
+
+        Console.WriteLine("Please input the third number: ");
+        string n3 = Console.ReadLine();
+        int n3_int = Convert.ToInt16(n3);
+
+        if(n1_int<n2_int){
+            if(n1_int<n3_int){
+                Console.WriteLine($"The smallest number is {n1_int}");
+            }
+            else{
+                Console.WriteLine($"The smallest number is {n3_int}");
+            }
+        }
+        else if(n2_int<n1_int){
+            if(n2_int<n3_int){
+                Console.WriteLine($"The smallest number is {n2_int}");
+            }
+            else{
+                Console.WriteLine($"The smallest number is {n3_int}");
+            }
+        }
+        else{
+            Console.WriteLine($"The smallest number is {n3_int}");
+        }
+
+    }
+}
